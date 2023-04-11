d3bed0a182c6ee55169a436f70ef52aed5191dcd
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-04-11 21:51:33 +0300

    добавил изменение цвета

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import './App.scss';
+import { useState } from 'react';
+import { quotes } from './quotes';
 
 function App() {
+  const [colorCount, setColorCount] = useState(0);
+  const [currentQuote, setCurrentQuote] = useState(quotes);
+  const colors = ["red", "orange", "violet", "green", "yellow", "blue", "brown"];
+
+  const changeColor = () => {
+    if (colorCount > 6) {
+      setColorCount(0)
+    } else {
+      setColorCount(colorCount + 1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${colors[colorCount]}`}>
+
+      <div id="quote-box">
+        <figure>
+          <blockquote >
+            <p id="text"></p>
+          </blockquote>
+          <figcaption id="author"></figcaption>
+        </figure>
+        <button id="new-quote" className={`${colors[colorCount]}`} onClick={changeColor}>New quote</button>
+      </div>
     </div>
   );
 }
